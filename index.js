56857c3cf56cb0ec5a18ff4891cc2be2add1eb49
Jannat150 <cshsjannat2987@gmail.com>
2025-01-30 09:19:47 +0530

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose=require('mongoose')
+require('dotenv').config();
+const URL=process.env.URL
 
 const app = express();
 const port = 3010;
+
 
 app.use(express.static('static'));
 
@@ -10,6 +14,12 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, async() => {
+  try{
+    await mongoose.connect(URL)
+    console.log("Connected to mongoDB");
+  }catch(err){
+    console.log("Error",err);
+  }
   console.log(`Example app listening at http://localhost:${port}`);
 });
